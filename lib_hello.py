44bf9c8322973d7530eb76c6267eaebfbdc90ad7
yungmuchen <yungmu.chen@gmail.com>
2016-06-11 19:23:51 +0800

    Update lib_hello.py

--- lib_hello.py
+++ lib_hello.py
@@ -1,25 +1,20 @@
 from pprint import pprint, pformat
 import os, platform
-
-def ping_check(host):
-    """
-    Returns True if host responds to a ping request
-    """
-    
-    # Ping parameters as function of OS
-    ping_str = "-n 1" if  platform.system().lower()=="windows" else "-c 1"
-
-    # Ping
-    ping_result = os.system("ping " + ping_str + " " + host) == 0
-    
-    if not ping_result:
-        raise Exception('ping failed')
-    else:
-        return True
-
+class MyPingLibrary:
+    def ping_check(self, host):
+        """
+        Returns True if host responds to a ping request
+        """
+        # Ping parameters as function of OS
+        ping_str = "-n 1" if  platform.system().lower()=="windows" else "-c 1"
+        # Ping
+        ping_result = os.system("ping " + ping_str + " " + host) == 0
+        if not ping_result:
+            raise Exception('ping failed')
+        else:
+            return True
 if __name__ == '__main__':
-  if ping_check('www.google.com'):
-      print 'ping pass'
-  
-  
-  
+    import MyPingLibrary as MPL
+    pinger = MPL.MyPingLibrary()
+    if pinger.ping_check('www.google.com'):
+        print 'ping pass'
